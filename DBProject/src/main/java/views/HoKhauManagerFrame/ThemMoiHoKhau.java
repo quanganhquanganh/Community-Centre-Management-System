146204fa6c5b363b0051c9282754c50618950896
NguyenAnhVuong <72128304+NguyenAnhVuong@users.noreply.github.com>
2021-05-27 13:08:02 +0700

    Update ThemMoiHoKhau.java

--- DBProject/src/main/java/views/HoKhauManagerFrame/ThemMoiHoKhau.java
+++ DBProject/src/main/java/views/HoKhauManagerFrame/ThemMoiHoKhau.java
@@ -12,10 +12,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import models.ThanhVienCuaHoModel;
 
-/**
- *
- * @author User
- */
+
 public class ThemMoiHoKhau extends javax.swing.JFrame {
 
     private JFrame parentJFrame;
