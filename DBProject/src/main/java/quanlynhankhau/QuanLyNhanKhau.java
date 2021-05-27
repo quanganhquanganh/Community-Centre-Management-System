28fe5f010d15a62f6f95c5cacfc8697bec5ba8d1
NguyenAnhVuong <72128304+NguyenAnhVuong@users.noreply.github.com>
2021-05-27 13:03:11 +0700

    Update QuanLyNhanKhau.java

--- DBProject/src/main/java/quanlynhankhau/QuanLyNhanKhau.java
+++ DBProject/src/main/java/quanlynhankhau/QuanLyNhanKhau.java
@@ -5,10 +5,7 @@ import javax.swing.UIManager;
 import views.mng.LogInView;
 import views.mng.MainFrame1;
 
-/**
- *
- * @author Hai
- */
+
 public class QuanLyNhanKhau {
 
     public static Calendar calendar = Calendar.getInstance();
