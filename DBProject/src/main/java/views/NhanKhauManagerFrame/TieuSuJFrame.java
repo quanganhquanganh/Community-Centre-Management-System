935fb3e8578a0ec04f0022606e7dd7b8478fb462
NguyenAnhVuong <72128304+NguyenAnhVuong@users.noreply.github.com>
2021-05-27 13:11:58 +0700

    Update TieuSuJFrame.java

--- DBProject/src/main/java/views/NhanKhauManagerFrame/TieuSuJFrame.java
+++ DBProject/src/main/java/views/NhanKhauManagerFrame/TieuSuJFrame.java
@@ -7,10 +7,7 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Hai
- */
+
 public class TieuSuJFrame extends javax.swing.JFrame {
 
     /**
