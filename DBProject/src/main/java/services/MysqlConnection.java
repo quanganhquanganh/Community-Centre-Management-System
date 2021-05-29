ab12e04852649d0a29e86327719a8ad4f33cfbe4
NTHMiLo <55952976+hieumilo1209@users.noreply.github.com>
2021-05-29 16:28:26 +0700

    Add files via upload

--- DBProject/src/main/java/services/MysqlConnection.java
+++ DBProject/src/main/java/services/MysqlConnection.java
@@ -4,10 +4,11 @@ import java.sql.*;
 
 public class MysqlConnection {
     public static Connection getMysqlConnection() throws SQLException, ClassNotFoundException {
-        String hostName = "localhost";
-        String dbName = "DBProject";
-        String userName = "root";
-        String password = "25102001";
+        String hostName = "sql6.freemysqlhosting.net";
+        String dbName = "sql6415471";
+        String userName = "sql6415471";
+        String password = "z9UTUxedgt";
+        System.out.print("Lol");
         return getMysqlConnection(hostName, dbName, userName, password);
     }
     
